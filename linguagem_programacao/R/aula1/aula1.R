1145cd294b6bbf0e8e1929ade78e4d1c811f67b1
Gabriel Figur Berger <gabriel.fberger@gmail.com>
2024-05-03 22:37:21 -0300

    [ADD] primeiros arquivos R

---
+++ linguagem_programacao/R/aula1/aula1.R
@@ -0,0 +1,34 @@
+# Aula 1
+
+2 + 2
+60 * 60
+
+x = 2
+y = 3
+
+ls()
+
+rm(x)
+rm(list = ls())
+
+search()
+women
+
+women = 123456
+women
+women = datasets::women
+women
+
+getwd()
+
+numeros = c(1,2,3,4,5,6,7,8,9)
+letras = c('a','b','c','d','e','f')
+
+as.numeric(letras)
+letras
+
+seq(from = 1,
+    to = 250,
+    by = 0.15)
+
+rep(c(1:4), each = 3, times = 5)
